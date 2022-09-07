3351a1b3e3aa61d86c2659aa690682bff55fa6ed
Andrii Polishchuk <poliskanrij68@gmail.com>
2022-09-07 19:03:22 +0300

    initial

---
+++ timetable_bot/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace timetable_bot;
+
+class Program{
+    public static void Main(){
+        DotEnv.Load(".env");
+        
+        var token = Environment.GetEnvironmentVariable("TOKEN");
+
+        if(token == null){
+            Console.WriteLine("Token not specified. Bot can't start");
+
+            return;
+        }
+        
+        Console.WriteLine(token);
+    }
+}
